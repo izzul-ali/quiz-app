73df1f63d870fd2751e2128c9ef0882c49290add
izzul-ali <izzulmaali3@gmail.com>
2023-07-11 21:50:44 +0700

    style: add new font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+
+export const poppins = Poppins({
+  weight: ['300', '400', '600', '700', '800'],
+  style: 'normal',
+  subsets: ['latin'],
+  display: 'auto',
+});
 
 export const metadata: Metadata = {
   title: 'Quiz App',
@@ -9,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      <body className={`bg-gray-50 ${poppins.className}`}>{children}</body>
     </html>
   );
 }
