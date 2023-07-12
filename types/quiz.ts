a70e8859943da339b6e4b75585bb6a95c3387b1c
izzul-ali <izzulmaali3@gmail.com>
2023-07-12 22:38:43 +0700

    chore: quiz schema

---
+++ types/quiz.ts
@@ -0,0 +1,13 @@
+export type Quiz = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionList = {
+  question: string;
+  answer: string[];
+};
