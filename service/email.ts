b557380a1992308eba70d543a5e2447f1030e65a
izzul-ali <izzulmaali3@gmail.com>
2023-07-17 21:27:24 +0700

    feat: change email verification code template

--- service/email.ts
+++ service/email.ts
@@ -15,19 +15,51 @@ export default function sendEmailVerification(email: string, code: string) {
     to: email,
     subject: 'Email Verification',
     html: `
-    <!doctype html>
+    <!DOCTYPE html>
     <html>
-    <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Code Email Verification</title>
-    <style>
     
-    </style>
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Verify your login</title>
+      <!--[if mso]><style type="text/css">body, table, td, a { font-family: Arial, Helvetica, sans-serif !important; }</style><![endif]-->
     </head>
-    <body>
-    <h1>${code}</h1>
+    
+    <body style="font-family: Helvetica, Arial, sans-serif; margin: 0px; padding: 0px; background-color: #ffffff;">
+      <table role="presentation"
+        style="width: 100%; border-collapse: collapse; border: 0px; border-spacing: 0px; font-family: Arial, Helvetica, sans-serif; background-color: rgb(239, 239, 239);">
+        <tbody>
+          <tr>
+            <td align="center" style="padding: 1rem 2rem; vertical-align: top; width: 100%;">
+              <table role="presentation" style="max-width: 600px; border-collapse: collapse; border: 0px; border-spacing: 0px; text-align: left;">
+                <tbody>
+                  <tr>
+                    <td style="padding: 40px 0px 0px;">
+                      <div style="text-align: left;">
+                        <div style="padding-bottom: 20px;"><img src="${process.env.NEXT_PUBLIC_APP_DOMAIN}/favicon-32x32.png" alt="Quiz App" style="width: 56px;"></div>
+                      </div>
+                      <div style="padding: 20px; background-color: rgb(255, 255, 255);">
+                        <div style="color: rgb(0, 0, 0); text-align: left;">
+                          <h1 style="margin: 1rem 0">Verification code</h1>
+                          <p style="padding-bottom: 16px">Please use the verification code below to enter the Quiz App. OTP is valid for 5 minutes.</p>
+                          <p style="padding-bottom: 16px"><strong style="font-size: 130%">${code}</strong></p>
+                          <p style="padding-bottom: 16px">If you didn’t request this, you can ignore this email.</p>
+                          <p style="padding-bottom: 16px">Thanks,<br>Izzul</p>
+                        </div>
+                      </div>
+                      <div style="padding-top: 20px; color: rgb(153, 153, 153); text-align: center;">
+                        <p style="padding-bottom: 16px">Made by Izzul</p>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </body>
+    
     </html>
     `,
   };
