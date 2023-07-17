9b59c134a9b5130c87475357404670b649a2eeea
izzul-ali <izzulmaali3@gmail.com>
2023-07-17 15:10:05 +0700

    added web icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,15 @@ export const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: 'Quiz App',
-  description: 'An example Quiz App based on web, built with React.js, Next.js and Tailwind CSS',
+  description: 'Example of a web-based Quiz App, built with React.js, Next.js, and Tailwind CSS',
   themeColor: '#eddbff',
+  icons: {
+    icon: [
+      { rel: 'icon', type: 'image/png', url: '/favicon-16x16.png', sizes: '16x16' },
+      { rel: 'icon', type: 'image/png', url: '/favicon-32x32.png', sizes: '32x32' },
+    ],
+    apple: { rel: 'apple-touch-icon', url: '/apple-touch-icon.png', sizes: '180x180' },
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
