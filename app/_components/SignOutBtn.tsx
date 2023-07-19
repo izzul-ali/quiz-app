61b5a5a872c7bd5f659bb9990c001b90da09eebe
izzul-ali <izzulmaali3@gmail.com>
2023-07-19 20:35:50 +0700

    feat: added loading spinner when logout

--- app/_components/SignOutBtn.tsx
+++ app/_components/SignOutBtn.tsx
@@ -1,11 +1,19 @@
 'use client';
 
+import { useTransition } from 'react';
+import { ImSpinner9 } from 'react-icons/im';
 import signout from '~/service/signout';
 
 export default function SignoutBtn() {
+  const [pending, startTransition] = useTransition();
+
   return (
-    <button onClick={() => signout()} className="px-3 py-1 text-xs bg-purple-600 text-white rounded">
-      Logout
+    <button
+      disabled={pending}
+      onClick={() => startTransition(async () => await signout())}
+      className="px-2 py-1 min-w-[4rem] block text-xs bg-purple-600 text-white rounded"
+    >
+      {pending ? <ImSpinner9 className="animate-spin mx-auto text-base fill-purple-200" /> : 'Logout'}
     </button>
   );
 }
